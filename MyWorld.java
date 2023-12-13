5a4c93ad588e12f630d66b8c4025f09cc0aa0207
Nich0las-Ng <341149722@gapps.yrdsb.ca>
2023-12-13 08:42:32 -0500

    name

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
+ * @author Nicholas Ng
  * @version (a version number or a date)
  */
 public class MyWorld extends World
