4bb7385d3559f3d6de01324b5fd384369be95a0a
Nich0las-Ng <341149722@gapps.yrdsb.ca>
2023-12-22 08:37:49 -0500

    Add gravaty and made it to where he can jump

--- Human.java
+++ Human.java
@@ -12,14 +12,36 @@ public class Human extends Actor
      * Act - do whatever the Human wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    static final int gravity = 2;
+    static final int jumpForce = 30;
+    int xSpeed = 4;
+    int ySpeed = 0;
+    
     public void act()
     {
         // Add your action code here.
         int x = getX();
         int y = getY();
-        while(y != 300)
+        
+        moveVertically();
+    }
+    
+    private void moveVertically()
+    {
+        int worldHeight = getWorld().getHeight();
+        int myHeight = getImage().getHeight();
+        boolean onGround = false;
+        ySpeed += gravity;
+        setLocation(getX(), getY()+ySpeed);
+        if(getY() > worldHeight-myHeight/2)
         {
-            move(-2);
+            ySpeed = 0;
+            onGround = true;
+        }
+        
+        if(onGround && Greenfoot.isKeyDown("space")) 
+        {
+            ySpeed =-jumpForce;
         }
     }
 }
